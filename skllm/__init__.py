a448b99da585cd83a7cf79416a27149304c48205
Oleh <kostromin97@gmail.com>
2023-05-29 20:16:25 +0200

    Added few-shot mc classifier

--- skllm/__init__.py
+++ skllm/__init__.py
@@ -1,1 +1,5 @@
-from skllm.models.gpt_zero_shot_clf import ZeroShotGPTClassifier, MultiLabelZeroShotGPTClassifier
+from skllm.models.gpt_few_shot_clf import FewShotGPTClassifier
+from skllm.models.gpt_zero_shot_clf import (
+    MultiLabelZeroShotGPTClassifier,
+    ZeroShotGPTClassifier,
+)
--- skllm/prompts/builders.py
+++ skllm/prompts/builders.py
@@ -1,6 +1,7 @@
 from typing import Union
 
 from skllm.prompts.templates import (
+    FEW_SHOT_CLF_PROMPT_TEMPLATE,
     SUMMARY_PROMPT_TEMPLATE,
     ZERO_SHOT_CLF_PROMPT_TEMPLATE,
     ZERO_SHOT_MLCLF_PROMPT_TEMPLATE,
@@ -29,6 +30,33 @@ def build_zero_shot_prompt_slc(
         prepared prompt
     """
     return template.format(x=x, labels=labels)
+
+
+def build_few_shot_prompt_slc(
+    x: str,
+    labels: str,
+    training_data: str,
+    template: str = FEW_SHOT_CLF_PROMPT_TEMPLATE,
+) -> str:
+    """Builds a prompt for zero-shot single-label classification.
+
+    Parameters
+    ----------
+    x : str
+        sample to classify
+    labels : str
+        candidate labels in a list-like representation
+    training_data : str
+        training data to be used for few-shot learning
+    template : str
+        prompt template to use, must contain placeholders for all variables, by default ZERO_SHOT_CLF_PROMPT_TEMPLATE
+
+    Returns
+    -------
+    str
+        prepared prompt
+    """
+    return template.format(x=x, labels=labels, training_data=training_data)
 
 
 def build_zero_shot_prompt_mlc(
