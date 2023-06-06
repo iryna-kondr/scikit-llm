30c857a79bacb89ee9c84b2843d8da4dab4924f5
Mohamad Zamini <32536264+mzamini92@users.noreply.github.com>
2023-06-06 14:58:08 -0600

    Update gpt_vectorizer.py
    Batch Processing: The code processes texts in batches of size batch_size, reducing the number of API calls and leveraging parallelism.
    
    Parallelization: The `joblib` library is used to parallelize the processing of individual texts within a batch. The Parallel function with `n_jobs=-1` is used to execute the `process_batch` function in parallel across multiple CPU cores.
    
    By combining batch processing and parallelization, the code can efficiently handle multiple texts in parallel and significantly improve performance when transforming a large number of texts.

--- skllm/preprocessing/gpt_vectorizer.py
+++ skllm/preprocessing/gpt_vectorizer.py
@@ -3,37 +3,52 @@ from typing import Any, List, Optional, Union
 import numpy as np
 import pandas as pd
 from numpy import ndarray
-from sklearn.base import BaseEstimator as _BaseEstimator
-from sklearn.base import TransformerMixin as _TransformerMixin
+from sklearn.base import BaseEstimator, TransformerMixin
+from joblib import Parallel, delayed
 from tqdm import tqdm
 
-from skllm.openai.embeddings import get_embedding as _get_embedding
-from skllm.openai.mixin import OpenAIMixin as _OAIMixin
-from skllm.utils import to_numpy as _to_numpy
+from skllm.openai.embeddings import get_embedding
+from skllm.openai.mixin import OpenAIMixin
+from skllm.utils import to_numpy
 
 
-class GPTVectorizer(_BaseEstimator, _TransformerMixin, _OAIMixin):
+class GPTVectorizer(BaseEstimator, TransformerMixin, OpenAIMixin):
     def __init__(
         self,
         openai_embedding_model: str = "text-embedding-ada-002",
         openai_key: Optional[str] = None,
         openai_org: Optional[str] = None,
+        batch_size: int = 32,
     ):
         self.openai_embedding_model = openai_embedding_model
         self._set_keys(openai_key, openai_org)
+        self.batch_size = batch_size
 
-    def fit(self, X: Any = None, y: Any = None, **kwargs):
+    def fit(self, X: Any = None, y: Any = None, **kwargs: Any):
         return self
 
     def transform(self, X: Optional[Union[np.ndarray, pd.Series, List[str]]]) -> ndarray:
-        X = _to_numpy(X)
+        X = to_numpy(X)
+        num_samples = len(X)
         embeddings = []
-        for i in tqdm(range(len(X))):
-            embeddings.append(
-                _get_embedding(X[i], self._get_openai_key(), self._get_openai_org())
+
+        def process_batch(batch_texts):
+            return get_embedding(
+                batch_texts,
+                self._get_openai_key(),
+                self._get_openai_org(),
+                model=self.openai_embedding_model,
             )
+
+        for i in tqdm(range(0, num_samples, self.batch_size)):
+            batch_texts = X[i : i + self.batch_size]
+            batch_embeddings = Parallel(n_jobs=-1)(
+                delayed(process_batch)(texts) for texts in batch_texts
+            )
+            embeddings.extend(batch_embeddings)
+
         embeddings = np.asarray(embeddings)
         return embeddings
 
-    def fit_transform(self, X: Optional[Union[np.ndarray, pd.Series, List[str]]], y=None, **fit_params) -> ndarray:
+    def fit_transform(self, X: Optional[Union[np.ndarray, pd.Series, List[str]]], y=None, **fit_params: Any) -> ndarray:
         return self.fit(X, y).transform(X)
