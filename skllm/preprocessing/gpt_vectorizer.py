1d24e4864f444af2b3332cd779d8eb868a711a8e
Mohamad Zamini <32536264+mzamini92@users.noreply.github.com>
2023-06-08 14:23:33 -0600

    Update gpt_vectorizer.py
    In this version, the code utilizes the `concurrent.futures.ThreadPoolExecutor()` to create a thread pool that allows concurrent execution of API calls for the batches. The `process_batch` function is defined to process a single batch by concatenating the batch samples into a single string and making an API call to get the batch embeddings.
    The loop iterates over the samples in batches and submits each batch to the executor using `executor.submit(process_batch, batch_texts)`, which returns a future object representing the asynchronous execution of the `process_batch` function for that batch.
    After submitting all the batches, the code uses `concurrent.futures.as_completed(batch_futures)` to iterate over the completed batch futures. When a batch future is completed, the corresponding batch embeddings are obtained using `batch_future.result()`, and they are appended to the embeddings list.
    Finally, the embeddings list is concatenated along the first axis using `np.concatenate` to obtain the final embeddings array.

--- skllm/preprocessing/gpt_vectorizer.py
+++ skllm/preprocessing/gpt_vectorizer.py
@@ -1,54 +1,108 @@
+from __future__ import annotations
+
 from typing import Any, List, Optional, Union
-from functools import partial
 
 import numpy as np
 import pandas as pd
 from numpy import ndarray
-from sklearn.base import BaseEstimator, TransformerMixin
-from joblib import Parallel, delayed
+from sklearn.base import BaseEstimator as _BaseEstimator
+from sklearn.base import TransformerMixin as _TransformerMixin
 from tqdm import tqdm
 
-from skllm.openai.embeddings import get_embedding
-from skllm.openai.mixin import OpenAIMixin
-from skllm.utils import to_numpy
+from skllm.openai.embeddings import get_embedding_batch as _get_embedding_batch
+from skllm.openai.mixin import OpenAIMixin as _OAIMixin
+from skllm.utils import to_numpy as _to_numpy
 
 
-class GPTVectorizer(BaseEstimator, TransformerMixin, OpenAIMixin):
+class GPTVectorizer(_BaseEstimator, _TransformerMixin, _OAIMixin):
+    """
+    A class that uses OPEN AI embedding model that converts text to GPT embeddings.
+
+    Parameters
+    ----------
+    openai_embedding_model : str
+        The OPEN AI embedding model to use. Defaults to "text-embedding-ada-002".
+    openai_key : str, optional
+        The OPEN AI key to use. Defaults to None.
+    openai_org : str, optional
+        The OPEN AI organization ID to use. Defaults to None.
+    """
     def __init__(
         self,
         openai_embedding_model: str = "text-embedding-ada-002",
         openai_key: Optional[str] = None,
         openai_org: Optional[str] = None,
-        batch_size: int = 32,
     ):
         self.openai_embedding_model = openai_embedding_model
         self._set_keys(openai_key, openai_org)
-        self.batch_size = batch_size
 
-    def fit(self, X: Any = None, y: Any = None, **kwargs: Any):
+    def fit(self, X: Any = None, y: Any = None, **kwargs) -> GPTVectorizer:
+        """
+        Fits the GPTVectorizer to the data.
+        This is modelled to function as the sklearn fit method.
+
+        Parameters
+        ----------
+        X : Any, optional
+        y : Any, optional
+        kwargs : dict, optional
+
+        Returns
+        -------
+        self : GPTVectorizer
+        """
         return self
 
     def transform(self, X: Optional[Union[np.ndarray, pd.Series, List[str]]]) -> ndarray:
-        X = to_numpy(X)
+        """
+        Transforms a list of strings into a list of GPT embeddings.
+        This is modelled to function as the sklearn transform method
+
+        Parameters
+        ----------
+        X : Optional[Union[np.ndarray, pd.Series, List[str]]]
+            The input array of strings to transform into GPT embeddings.
+
+        Returns
+        -------
+        embeddings : np.ndarray
+        """
+        X = _to_numpy(X)
+        batch_size = 16  # Number of samples to process in each batch
         num_samples = len(X)
         embeddings = []
 
-        def process_texts(texts):
-            return get_embedding(
-                texts,
-                self._get_openai_key(),
-                self._get_openai_org(),
-                model=self.openai_embedding_model,
-            )
+        def process_batch(batch_texts):
+            batch_text = ' '.join(batch_texts)  # Concatenate batch samples into a single string
+            return _get_embedding(batch_text, self._get_openai_key(), self._get_openai_org())
 
-        with Parallel(n_jobs=-1) as parallel:
-            for i in tqdm(range(0, num_samples, self.batch_size)):
-                batch_texts = X[i: i + self.batch_size]
-                batch_embeddings = parallel(delayed(process_texts)(texts) for texts in batch_texts)
-                embeddings.extend(batch_embeddings)
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            batch_futures = []
+            for i in range(0, num_samples, batch_size):
+                batch_texts = X[i:i+batch_size]
+                batch_future = executor.submit(process_batch, batch_texts)
+                batch_futures.append(batch_future)
 
-        embeddings = np.asarray(embeddings)
+            for batch_future in concurrent.futures.as_completed(batch_futures):
+                batch_embeddings = batch_future.result()
+                embeddings.append(batch_embeddings)
+
+        embeddings = np.concatenate(embeddings, axis=0)
         return embeddings
 
-    def fit_transform(self, X: Optional[Union[np.ndarray, pd.Series, List[str]]], y=None, **fit_params: Any) -> ndarray:
+    def fit_transform(self, X: Optional[Union[np.ndarray, pd.Series, List[str]]], y=None, **fit_params) -> ndarray:
+        """
+        Fits and transforms a list of strings into a list of GPT embeddings.
+        This is modelled to function as the sklearn fit_transform method
+
+        Parameters
+        ----------
+        X : Optional[Union[np.ndarray, pd.Series, List[str]]]
+            The input array of strings to transform into GPT embeddings.
+        y : Any, optional
+
+        Returns
+        -------
+        embeddings : np.ndarray
+        """
         return self.fit(X, y).transform(X)
